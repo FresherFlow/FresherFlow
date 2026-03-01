29cf1d6fb2f6a2e0689b02b571c5b85a247e4631
MukeshCheekatla <cheekatlamukesh@gmail.com>
2026-03-02 04:28:54 +0530

    web: add robots policy to reduce crawler load on private and api routes

---
+++ apps/web/src/app/robots.ts
@@ -0,0 +1,40 @@
+import type { MetadataRoute } from 'next';
+
+const PUBLIC_WEB_HOST = process.env.PUBLIC_WEB_HOST || 'fresherflow.in';
+
+export default function robots(): MetadataRoute.Robots {
+    const host = /^https?:\/\//i.test(PUBLIC_WEB_HOST) ? PUBLIC_WEB_HOST : `https://${PUBLIC_WEB_HOST}`;
+
+    return {
+        rules: [
+            {
+                userAgent: '*',
+                allow: [
+                    '/',
+                    '/jobs',
+                    '/internships',
+                    '/walk-ins',
+                    '/opportunities',
+                ],
+                disallow: [
+                    '/api/',
+                    '/admin/',
+                    '/dashboard/',
+                    '/account/',
+                    '/profile/',
+                    '/alerts/',
+                ],
+            },
+            {
+                userAgent: 'GPTBot',
+                disallow: ['/'],
+            },
+            {
+                userAgent: 'CCBot',
+                disallow: ['/'],
+            },
+        ],
+        sitemap: `${host.replace(/\/+$/, '')}/sitemap.xml`,
+        host: host.replace(/\/+$/, ''),
+    };
+}
