d84545eb1751c8eaf0986de8d61f3526fafd8148
MukeshCheekatla <cheekatlamukesh@gmail.com>
2026-07-05 19:37:47 +0530

    chore: remove legacy ingestion workspace and redundant worker processors

--- apps/worker/src/jobs/opportunity/handleExpired.ts
+++
@@ -1,9 +0,0 @@
-import { logger } from '@fresherflow/logger';
-
-/**
- * Handle Opportunity Expired background jobs.
- */
-export async function handleExpired(opportunityId: string) {
-    logger.info('[Worker] Expiring opportunity', { opportunityId });
-    // add database cleanup or alert events triggers here
-}
